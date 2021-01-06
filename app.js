5141fa2b7a8f3a45ae2a120f503e6bb5350369ba
Nithin Sai K J <nithins674@gmail.com>
2021-01-06 21:25:36 +0530

    removed db import

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var database = require('./database');
-const { errorLog } = require('./errorHandle');
+// var database = require('./database');
+// const { errorLog } = require('./errorHandle');
 
 var app = express();
 
